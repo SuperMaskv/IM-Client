724b2c1eb4d1fe4ff67bf1fe557053a540bf0533
SuperMaskv <supermaskv@foxmail.com>
2020-01-31 22:29:09 +0800

    Refactor

---
+++ IM-Client/Utils/ViewModelLocator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace IM_Client.Utils
+{
+    public class ViewModelLocator
+    {
+        private UnityContainer container;
+
+        public ViewModelLocator()
+        {
+            container=new UnityContainer();
+            container.RegisterType<>()
+        }
+    }
+}
